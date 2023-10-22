c5d705eb213dd39a0de0c7a1232df3cee480c3bc
gaurav-132 <ggusain264@gmail.com>
2023-10-22 09:52:33 +0530

    Local Changes in Secondary Container

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -8,7 +8,7 @@ const SecondaryContainer = () => {
 
     return (
         <div className='bg-black'>
-            <div className='mt-0 md:-mt-52 pl-2 md:pl-6 relative z-20'>
+            <div className='mt-0 md:-mt-10 pl-2 md:pl-6 relative z-20'>
                 <MovieList title={"Now Playing Movies"} movies={movies.nowPlayingMovies}/>
                 <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/>
                 <MovieList title={"Popular"} movies={movies.popularMovies}/>
